e710178e52782341a4dbb9a7c204d2abfad955fd
ashish <ashishkdsia@gmail.com>
2021-02-26 02:50:49 -0600

    Database++

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,15 @@
-from flask import Blueprint
-
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from . import db
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    data = request.form
+    print(data)
+    return render_template('login.html')
 
 
 @auth.route('/logout')
@@ -13,7 +17,25 @@ def logout():
     return "<p>logout</p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if password1 != password2:
+            flash('Passwords can\'t be same', category='error')
+        elif len(password1) < 7:
+            flash('Passwords must be at least 7 characters long.', category='error')
+        else:
+
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Successfully Signed Up', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template('signUp.html')
 
